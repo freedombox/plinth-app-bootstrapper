c20054cf52241efd2ef0c4df5e70bc938f84a83f
Joseph Nuthalpati <njoseph@thoughtworks.com>
2017-06-17 22:54:25 +0530

    Use templates
    - Using templates with common functions to reduce repetitive work.

---
+++ templates/module/__init__.py
@@ -0,0 +1,125 @@
+#
+# This file is part of Plinth.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+Plinth module to configure $app.
+"""
+
+from django.utils.translation import ugettext_lazy as _
+
+from plinth import actions
+from plinth import action_utils
+from plinth import cfg
+from plinth import frontpage
+from plinth import service as service_module
+from plinth.menu import main_menu
+from plinth.utils import format_lazy
+
+
+version = 1
+
+managed_services = ['$app']
+
+managed_packages = ['$app']
+
+title = _('')
+
+description = [
+    _(""),
+]
+
+service = None
+
+
+def init():
+    """Intialize the module."""
+    menu = main_menu.get('apps')
+    menu.add_urlname(title, 'glyphicon-refresh', '$app:index')
+
+    global service
+    setup_helper = globals()['setup_helper']
+    if setup_helper.get_state() != 'needs-setup':
+        service = service_module.Service(
+            managed_services[0],
+            title,
+            ports=['http', 'https'],
+            is_external=True,
+            is_enabled=is_enabled,
+            enable=enable,
+            disable=disable,
+            is_running=is_running)
+
+        if is_enabled():
+            add_shortcut()
+
+
+def setup(helper, old_version=None):
+    """Install and configure the module."""
+    helper.install(managed_packages)
+    helper.call('post', actions.superuser_run, '$app', ['enable'])
+    global service
+    if service is None:
+        service = service_module.Service(
+            managed_services[0],
+            title,
+            ports=['http', 'https'],
+            is_external=True,
+            is_enabled=is_enabled,
+            enable=enable,
+            disable=disable,
+            is_running=is_running)
+    helper.call('post', service.notify_enabled, None, True)
+    helper.call('post', add_shortcut)
+
+
+def add_shortcut():
+    """Helper method to add a shortcut to the frontpage."""
+    frontpage.add_shortcut(
+        '$app', title, url='/$app', login_required=True)
+
+
+def is_running():
+    """Return whether the service is running."""
+    return action_utils.service_is_running('$app')
+
+
+def is_enabled():
+    """Return whether the module is enabled."""
+    return (action_utils.service_is_enabled('$app') and
+            action_utils.webserver_is_enabled('$app-plinth'))
+
+
+def enable():
+    """Enable the module."""
+    actions.superuser_run('$app', ['enable'])
+    add_shortcut()
+
+
+def disable():
+    """Enable the module."""
+    actions.superuser_run('$app', ['disable'])
+    frontpage.remove_shortcut('$app')
+
+
+def diagnose():
+    """Run diagnostics and return the results."""
+    results = []
+
+    results.extend(
+        action_utils.diagnose_url_on_all(
+            'https://{host}/$app', check_certificate=False))
+
+    return results
--- templates/module/urls.py
+++ templates/module/urls.py
@@ -1,5 +1,5 @@
 #
-# This file is part of plinth-app-bootstrapper.
+# This file is part of Plinth.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -14,6 +14,21 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+"""
+URLs for the $app module.
+"""
 
+from django.conf.urls import url
 
-plinth_home = '/Users/njoseph/dev/freedombox/Plinth'
+from plinth.views import ServiceView
+from plinth.modules import $app
+
+urlpatterns = [
+    url(r'^apps/$app/$',
+        ServiceView.as_view(
+            service_id=$app.managed_services[0],
+            diagnostics_module_name='$app',
+            description=$app.description,
+            show_status_block=True),
+        name='index'),
+]
